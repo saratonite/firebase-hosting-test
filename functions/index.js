21505b756982f17b8ae5a37144b2a2736e997d1c
Sarath <sarath.hacks@gmail.com>
2017-11-17 12:15:56 +0530

    Added express app

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,37 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
+const firebase = require('firebase-admin')
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const firebaseApp = firebase.initializeApp( functions.config().firebase ) 
+
+function getQuotes() {
+    const ref = firebaseApp.database().ref('qoutes')
+
+    return ref.once('value').then( snap => snap.val())
+}
+
+const express = require('express')
+
+const application = express()
+
+application.get('/app', (req,res) => {
+
+    let possibilities = [true, false];
+    let _value = possibilities[Math.floor(Math.random() * possibilities.length)]
+
+    let data = {
+        luck: _value,
+        timestamp: Date.now()
+    }
+    //
+
+    getQuotes().then(data => {
+
+         res.json(data)
+
+    })
+   
+})
+
+exports.app = functions.https.onRequest(application);
